a8439ac7f1c96251ef61ddfdaef994a65a77f0ef
rosun <edglobalsolutions@gmail.com>
2023-05-09 16:50:04 +0300

    GoQuantum page defined

--- qb-frontend/src/components/GoQuantum.js
+++ qb-frontend/src/components/GoQuantum.js
@@ -1,31 +1,46 @@
 import React from 'react';
-import Navbar from "./Navbar";
+import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText,MDBBtn } from 'mdb-react-ui-kit';
 import Footer from "./Footer";
-import SlimSidebar from "./SlimSidebar";
+import Navbar from "./Navbar";
 
 function GoQuantum() {
     return (
         <div>
         <Navbar />
-            <SlimSidebar />
-        <div className='container'>
-            <div className='row'>
-                <div className='col-md-6'>
-                    <h2>Column 1</h2>
-                    <p>
-                        asd asdasdasdasd
-                    </p>
-                </div>
-                <div className='col-md-6'>
-                    <h2>Column 2</h2>
-                    <p>
-                       asdasdasdasdasdasda
+            <div className="home-page">
+                <h1>Welcome to QuantumBuy</h1>
+                <p>At QuantumBuy, we're all about making bulk buying and selling easy and affordable. Join us and start saving today!</p>
+                <div className="row">
+                    <div className="col-md-6 col-lg-6">
+                        <MDBCard style={{
+                            backgroundImage: `url(${require('./pictures/buypic.jpg')})`,
+                            backgroundSize: 'cover',
+                            height: '250%'
+                        }}>
+                            <MDBCardBody className="text-center">
+                                <MDBBtn color="primary" size="lg">Q - BUY</MDBBtn>
+                                <MDBCardText className='mt-3' style={{ color: '#fff' }}><h3>Start buying now</h3></MDBCardText>
+                                <MDBCardText className='mt-3' style={{ color: '#fff' }}><h4>Our selection of products are carefully curated and sourced from the best suppliers.</h4></MDBCardText>
+                            </MDBCardBody>
+                        </MDBCard>
+                    </div>
+                    <div className="col-md-6 col-lg-6">
+                        <MDBCard style={{
+                            backgroundImage: `url(${require('./pictures/sellpic.jpg')})`,
+                            backgroundSize: 'cover',
+                            height: '250%'
+                        }}>
+                            <MDBCardBody className="text-center">
+                                <MDBBtn color="primary" size="lg">Q - Sell</MDBBtn>
+                                <MDBCardText className='mt-3' style={{ color: '#fff' }}><h3>Start Selling Now</h3></MDBCardText>
+                                <MDBCardText className='mt-3' style={{ color: '#fff' }}><h4>By selling in bulk, we're able to help you reach a wider audience and increase your profits.</h4></MDBCardText>
+                            </MDBCardBody>
+                        </MDBCard>
+                    </div>
                 </div>
             </div>
             <Footer />
         </div>
-        </div>
-
     );
 }
 
